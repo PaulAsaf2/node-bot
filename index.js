052c0ba746d78c8599bbe02b559d320cd96fed2a
Pavel Asafov <pavelaof@gmail.com>
2024-03-08 17:48:23 +0300

    add webapp-data route

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express')
 const cors = require('cors')
 
 const bot = new Bot('6939027276:AAEOHc-hUEdScVXCMVtCVbal05X_gaNoNIo')
-
 const app = express()
 
 app.use(express.json())
@@ -11,5 +10,17 @@ app.use(cors())
 
 bot.command('start', ctx => ctx.reply('Welcome! Up and running.'))
 bot.on('message', ctx => ctx.reply('Got another message!'))
+
+app.post('/webapp-data', async (req, res) => {
+  const { queryId, title } = req.body
+  await bot.answerWebAppQuery(queryId, {
+    type: 'article',
+    id: queryId,
+    title: 'Это заголовок сообщения',
+    input_message_content: { message_text: title }
+  })
+
+  return res.status(200).send({ message: 'Succeed' })
+})
 
 bot.start()
